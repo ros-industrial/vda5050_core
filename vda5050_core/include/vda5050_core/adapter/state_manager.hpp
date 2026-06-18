a2a79486b04d56d2f5e4574dbfdcfdd405100a5a
Saurabh Kamat <saurabh_kamat@a-star.edu.sg>
2026-06-18 15:47:46 +0800

    feat: add new methods to state manager to let adapter mutate state
    Signed-off-by: Saurabh Kamat <saurabh_kamat@a-star.edu.sg>

--- vda5050_core/include/vda5050_core/adapter/state_manager.hpp
+++ vda5050_core/include/vda5050_core/adapter/state_manager.hpp
@@ -28,10 +28,9 @@
 #include "vda5050_core/types/battery_state.hpp"
 #include "vda5050_core/types/error.hpp"
 #include "vda5050_core/types/operating_mode.hpp"
+#include "vda5050_core/types/order.hpp"
 #include "vda5050_core/types/safety_state.hpp"
 #include "vda5050_core/types/state.hpp"
-
-#include "vda5050_core/adapter/execution_progress.hpp"
 
 namespace vda5050_core {
 
@@ -42,24 +41,49 @@ class StateManager : public std::enable_shared_from_this<StateManager>
 public:
   static std::shared_ptr<StateManager> make();
 
+  void set_agv_position(const types::AGVPosition& position);
+
+  void set_velocity(const types::Velocity& velocity);
+
   void set_driving(bool driving);
 
-  void set_agv_position(const types::AGVPosition& position);
+  void set_paused(bool paused);
+
+  void set_new_base_request(bool request);
+
+  void set_distance_since_last_node(double distance);
 
   void set_battery_state(const types::BatteryState& battery);
 
+  void set_operating_mode(types::OperatingMode mode);
+
+  void set_safety_state(const types::SafetyState& safety_state);
+
+  void add_action_state(const types::ActionState& action_state);
+
   void set_action_states(const std::vector<types::ActionState>& action_states);
 
-  void set_operating_mode(types::OperatingMode mode);
+  void clear_action_states();
 
   void add_error(const types::Error& error);
 
+  void set_errors(const std::vector<types::Error>& errors);
+
   void clear_errors();
 
-  void set_execution_progress(
-    std::shared_ptr<ExecutionProgress> execution_progress);
+  void add_load(const types::Load& load);
 
-  void set_safety_state(const types::SafetyState& safety_state);
+  void set_loads(const std::vector<types::Load>& loads);
+
+  void clear_loads();
+
+  void remove_loads();
+
+  void add_information(const types::Info& information);
+
+  void set_information(const std::vector<types::Info>& infomation);
+
+  void remove_information();
 
   types::State state() const;
 
@@ -68,7 +92,17 @@ public:
   bool consume_publish_requested();
 
 private:
+  friend class Adapter;
+
   StateManager();
+
+  void set_order(const types::Order& order);
+
+  void node_reached(const types::Node& node);
+
+  void edge_traversed(const types::Edge& edge);
+
+  void clear_order();
 
   mutable std::mutex mutex_;
   types::State state_;
--- vda5050_core/src/adapter/state_manager.cpp
+++ vda5050_core/src/adapter/state_manager.cpp
@@ -16,6 +16,8 @@
  * limitations under the License.
  */
 
+#include <algorithm>
+
 #include "vda5050_core/adapter/state_manager.hpp"
 
 namespace vda5050_core {
@@ -30,6 +32,20 @@ std::shared_ptr<StateManager> StateManager::make()
 }
 
 //=============================================================================
+void StateManager::set_agv_position(const types::AGVPosition& position)
+{
+  std::lock_guard<std::mutex> lock(mutex_);
+  state_.agv_position = position;
+}
+
+//=============================================================================
+void StateManager::set_velocity(const types::Velocity& velocity)
+{
+  std::lock_guard<std::mutex> lock(mutex_);
+  state_.velocity = velocity;
+}
+
+//=============================================================================
 void StateManager::set_driving(bool driving)
 {
   std::lock_guard<std::mutex> lock(mutex_);
@@ -38,10 +54,26 @@ void StateManager::set_driving(bool driving)
 }
 
 //=============================================================================
-void StateManager::set_agv_position(const types::AGVPosition& position)
+void StateManager::set_paused(bool paused)
 {
   std::lock_guard<std::mutex> lock(mutex_);
-  state_.agv_position = position;
+  state_.paused = paused;
+  publish_requested_ = true;
+}
+
+//=============================================================================
+void StateManager::set_new_base_request(bool request)
+{
+  std::lock_guard<std::mutex> lock(mutex_);
+  state_.new_base_request = request;
+  publish_requested_ = true;
+}
+
+//=============================================================================
+void StateManager::set_distance_since_last_node(double distance)
+{
+  std::lock_guard<std::mutex> lock(mutex_);
+  state_.distance_since_last_node = distance;
 }
 
 //=============================================================================
@@ -52,16 +84,42 @@ void StateManager::set_battery_state(const types::BatteryState& battery)
 }
 
 //=============================================================================
-void StateManager::set_action_states(
-  const std::vector<types::ActionState>& action_states)
-{
-}
-
-//=============================================================================
 void StateManager::set_operating_mode(types::OperatingMode mode)
 {
   std::lock_guard<std::mutex> lock(mutex_);
   state_.operating_mode = mode;
+}
+
+//=============================================================================
+void StateManager::set_safety_state(const types::SafetyState& safety_state)
+{
+  std::lock_guard<std::mutex> lock(mutex_);
+  state_.safety_state = safety_state;
+  publish_requested_ = true;
+}
+
+//=============================================================================
+void StateManager::add_action_state(const types::ActionState& action_state)
+{
+  std::lock_guard<std::mutex> lock(mutex_);
+  state_.action_states.push_back(action_state);
+  publish_requested_ = true;
+}
+
+//=============================================================================
+void StateManager::set_action_states(
+  const std::vector<types::ActionState>& action_states)
+{
+  std::lock_guard<std::mutex> lock(mutex_);
+  state_.action_states = action_states;
+  publish_requested_ = true;
+}
+
+//=============================================================================
+void StateManager::clear_action_states()
+{
+  std::lock_guard<std::mutex> lock(mutex_);
+  state_.action_states.clear();
 }
 
 //=============================================================================
@@ -73,6 +131,14 @@ void StateManager::add_error(const types::Error& error)
 }
 
 //=============================================================================
+void StateManager::set_errors(const std::vector<types::Error>& errors)
+{
+  std::lock_guard<std::mutex> lock(mutex_);
+  state_.errors = errors;
+  publish_requested_ = true;
+}
+
+//=============================================================================
 void StateManager::clear_errors()
 {
   std::lock_guard<std::mutex> lock(mutex_);
@@ -80,7 +146,69 @@ void StateManager::clear_errors()
 }
 
 //=============================================================================
-types::State state() {}
+void StateManager::add_load(const types::Load& load)
+{
+  std::lock_guard<std::mutex> lock(mutex_);
+
+  if (!state_.loads.has_value())
+  {
+    state_.loads = {};
+  }
+  state_.loads->push_back(load);
+}
+
+//=============================================================================
+void StateManager::set_loads(const std::vector<types::Load>& loads)
+{
+  std::lock_guard<std::mutex> lock(mutex_);
+  state_.loads = loads;
+}
+
+//=============================================================================
+void StateManager::clear_loads()
+{
+  std::lock_guard<std::mutex> lock(mutex_);
+  state_.loads = {};
+}
+
+//=============================================================================
+void StateManager::remove_loads()
+{
+  std::lock_guard<std::mutex> lock(mutex_);
+  state_.loads = std::nullopt;
+}
+
+//=============================================================================
+void StateManager::add_information(const types::Info& information)
+{
+  std::lock_guard<std::mutex> lock(mutex_);
+  if (!state_.information.has_value())
+  {
+    state_.information = {};
+  }
+  state_.information->push_back(information);
+}
+
+//=============================================================================
+void StateManager::set_information(const std::vector<types::Info>& infomation)
+{
+  std::lock_guard<std::mutex> lock(mutex_);
+  state_.information = infomation;
+}
+
+//=============================================================================
+void StateManager::remove_information()
+{
+  std::lock_guard<std::mutex> lock(mutex_);
+  state_.information = std::nullopt;
+}
+
+//=============================================================================
+types::State StateManager::state() const
+{
+  std::lock_guard<std::mutex> lock(mutex_);
+  return state_;
+}
 
 //=============================================================================
 void StateManager::mark_publish_requested()
@@ -91,13 +219,102 @@ void StateManager::mark_publish_requested()
 //=============================================================================
 bool StateManager::consume_publish_requested()
 {
-  publish_requested_.exchange(false);
+  return publish_requested_.exchange(false);
 }
 
 //=============================================================================
 StateManager::StateManager() : publish_requested_(false)
 {
   // Nothing to do here ...
+}
+
+//=============================================================================
+void StateManager::set_order(const types::Order& order)
+{
+  std::lock_guard<std::mutex> lock(mutex_);
+
+  state_.order_id = order.order_id;
+  state_.order_update_id = order.order_update_id;
+  state_.zone_set_id = order.zone_set_id;
+
+  state_.node_states.clear();
+  state_.edge_states.clear();
+
+  for (const auto& node : order.nodes)
+  {
+    types::NodeState ns;
+    ns.node_id = node.node_id;
+    ns.sequence_id = node.sequence_id;
+    ns.released = node.released;
+    ns.node_description = node.node_description;
+    ns.node_position = node.node_position;
+
+    state_.node_states.push_back(std::move(ns));
+  }
+
+  for (const auto& edge : order.edges)
+  {
+    types::EdgeState es;
+    es.edge_id = edge.edge_id;
+    es.sequence_id = es.sequence_id;
+    es.released = edge.released;
+    es.edge_description = edge.edge_description;
+    es.trajectory = edge.trajectory;
+
+    state_.edge_states.push_back(std::move(es));
+  }
+}
+
+//=============================================================================
+void StateManager::node_reached(const types::Node& node)
+{
+  std::lock_guard<std::mutex> lock(mutex_);
+
+  state_.last_node_id = node.node_id;
+  state_.last_node_sequence_id = node.sequence_id;
+
+  auto it = std::find_if(
+    state_.node_states.begin(), state_.node_states.end(),
+    [&](const auto& node_state) {
+      return node_state.node_id == node.node_id &&
+             node_state.sequence_id == node.sequence_id;
+    });
+
+  if (it != state_.node_states.end())
+  {
+    state_.node_states.erase(it);
+  }
+}
+
+//=============================================================================
+void StateManager::edge_traversed(const types::Edge& edge)
+{
+  std::lock_guard<std::mutex> lock(mutex_);
+
+  auto it = std::find_if(
+    state_.edge_states.begin(), state_.edge_states.end(),
+    [&](const auto& edge_state) {
+      return edge_state.edge_id == edge.edge_id && edge_state.sequence_id =
+               edge.sequence_id;
+    });
+
+  if (it != state_.edge_states.end())
+  {
+    state_.edge_states.erase(it);
+  }
+}
+
+//=============================================================================
+void StateManager::clear_order()
+{
+  std::lock_guard<std::mutex> lock(mutex_);
+
+  state_.node_states.clear();
+  state_.edge_states.clear();
+
+  state_.order_id.clear();
+  state_.order_update_id = 0;
+  state_.zone_set_id.reset();
 }
 
 }  // namespace adapter
